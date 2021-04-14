983e31a5ea7db0e57d91a2ed25cf21576abd4dbb
mohax <mohax.spb@gmail.com>
2021-04-14 15:47:02 +0300

    Add Huawei plugin. Remove webview related code.

--- app/src/main/java/ru/kuchanov/nightmode/MainActivity.kt
+++ app/src/main/java/ru/kuchanov/nightmode/MainActivity.kt
@@ -1,13 +1,8 @@
 package ru.kuchanov.nightmode
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.webkit.WebSettingsCompat
-import androidx.webkit.WebViewFeature
 import ru.kuchanov.nightmode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -21,20 +16,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.textView.setOnClickListener { switchAppTheme() }
-
-        initWebView()
-
-        setWebViewTheme()
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    private fun initWebView() {
-        binding.webView.webViewClient = WebViewClient()
-        val webSettings = binding.webView.settings
-        webSettings.javaScriptEnabled = true
-        webSettings.domStorageEnabled = true
-
-        binding.webView.loadUrl("https://vk.com/")
     }
 
     private fun switchAppTheme() {
@@ -44,23 +25,6 @@ class MainActivity : AppCompatActivity() {
             AppCompatDelegate.MODE_NIGHT_YES
         }
         AppCompatDelegate.setDefaultNightMode(appTheme)
-    }
-
-    private fun setWebViewTheme() {
-        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
-            val themeForWebView = if (isNight()) {
-                WebSettingsCompat.FORCE_DARK_ON
-            } else {
-                WebSettingsCompat.FORCE_DARK_OFF
-            }
-            WebSettingsCompat.setForceDark(binding.webView.settings, themeForWebView)
-        } else {
-            Toast.makeText(
-                this,
-                "WebViewFeature.FORCE_DARK is not supported!",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
     }
 
     private fun isNight(): Boolean {
